0cff1334e23959d4b20f1e0a0372bbdda4e4394c
Jeremy Postlethwaite <jeremy.postlethwaite@gmail.com>
2025-08-30 11:39:20 -0700

    v74.0.0

--- src/Building/BuildModel.php
+++ src/Building/BuildModel.php
@@ -24,6 +24,7 @@ trait BuildModel
         $revision = $this->hasOption('revision') && $this->option('revision');
 
         $this->build_detail_flags($this->model);
+        $this->build_detail_manage_blades();
         $this->build_detail_manage_ids($this->model);
 
         // dd([
@@ -42,7 +43,7 @@ trait BuildModel
 
         $blades['detail.blade.php'] = 'blade/playground/resource/model/detail.blade.php.stub';
         if ($revision) {
-            $blades['form-with-revision.blade.php'] = 'blade/playground/resource/model/form.blade.php.stub';
+            $blades['form.blade.php'] = 'blade/playground/resource/model/form-with-revision.blade.php.stub';
         } else {
             $blades['form.blade.php'] = 'blade/playground/resource/model/form.blade.php.stub';
         }
@@ -65,7 +66,7 @@ trait BuildModel
             $destination = sprintf(
                 '%1$s/%2$s%3$s',
                 $this->folder(),
-                $this->c->folder() ? $this->c->folder().'/' : '',
+                $this->c->folder() ? $this->c->folder() . '/' : '',
                 $blade
             );
             // dd([
@@ -80,10 +81,63 @@ trait BuildModel
 
             $this->search_and_replace($stub);
 
-            $full_path = $this->laravel->storagePath().$destination;
+            $full_path = $this->laravel->storagePath() . $destination;
             $this->files->put($full_path, $stub);
 
             $this->components->info(sprintf('Blade: %s [%s] created successfully.', $blade, $full_path));
         }
+    }
+
+    protected function build_detail_manage_blades(): void
+    {
+        // TODO this does not need to be build on every model
+
+        $pathToIo = $this->laravel->storagePath().$this->getPackageFolder().'/resources/views/io';
+//        dd([
+//            '__METHOD__' => __METHOD__,
+//            '$pathToIo' => $pathToIo,
+//            '$this->folder' => $this->folder(),
+//        ]);
+        if (!is_dir($pathToIo)) {
+            mkdir($pathToIo);
+        }
+
+        /**
+         * @var array<string, string> $blades
+         */
+        $blades = [];
+
+        $blades['manage-matrix.blade.php'] = 'blade/playground/resource/io/manage-matrix.blade.php';
+        $blades['manage-owner.blade.php'] = 'blade/playground/resource/io/manage-owner.blade.php';
+        $blades['manage-parent.blade.php'] = 'blade/playground/resource/io/manage-parent.blade.php';
+
+        foreach ($blades as $blade => $source) {
+
+            // $path_stub = 'blade'.$blade;
+            $path = $this->resolveStubPath($source);
+
+            $destination = sprintf(
+                '%1$s/io/%2$s',
+                $this->folder(),
+                $blade
+            );
+//             dd([
+//                 '__METHOD__' => __METHOD__,
+//                 '$source' => $source,
+//                 '$path' => $path,
+//                 '$destination' => $destination,
+//                 '$this->folder' => $this->folder(),
+//                 '$this->c' => $this->c,
+//             ]);
+            $stub = $this->files->get($path);
+
+            $this->search_and_replace($stub);
+
+            $full_path = $this->laravel->storagePath() . $destination;
+            $this->files->put($full_path, $stub);
+
+            $this->components->info(sprintf('Blade: %s [%s] created successfully.', $blade, $full_path));
+        }
+
     }
 }
